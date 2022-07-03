93a3ce4fbe34ac729b5a23b1f929e995c566fb58
Manh Quan <manhquan.05012002@gmail.com>
2022-07-03 18:01:03 +0700

    nhanh master

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
-import Form from "./components/form/Form";
-import Todo from "./components/todo/Todo";
-import { useSelector } from "react-redux";
-
 function App() {
-  const taskItem = useSelector((state) => state.taskReducer.taskItem);
-
-  return (
-    <div className="App">
-      <Todo>
-        <Form taskItem={taskItem}></Form>
-      </Todo>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
